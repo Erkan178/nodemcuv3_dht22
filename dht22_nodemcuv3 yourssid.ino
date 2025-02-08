17901b092a14b4de684b0043fa8969f006821cc7
Erkan178 <52273461+Erkan178@users.noreply.github.com>
2025-02-08 13:24:28 +0300

    Add files via upload
    dht22 and WiFi nodemcu v3 sensor reading project

---
+++ dht22_nodemcuv3 yourssid.ino
@@ -0,0 +1,54 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <DHT.h>
+
+#define DHTPIN D4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  
+  Serial.println("Attempting to connect to WiFi...");
+  WiFi.begin("your ssid", "your password"); 
+
+  int retries = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    retries++;
+    if (retries > 60) { // 60 * 500ms = 30 seconds
+      Serial.println("Failed to connect to WiFi");
+      return;
+    }
+  }
+
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Web server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handleRoot() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if (isnan(h) || isnan(t)) {
+    server.send(500, "text/plain", "Sensor error");
+    return;
+  }
+
+  String message = "Temperature: " + String(t) + " C\n";
+  message += "Humidity: " + String(h) + " %\n";
+  server.send(200, "text/plain", message);
+}
